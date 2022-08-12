9815258e08b48d607f758b2a149cbfcac5aafefe
Divya Tomar <divya.tomar@hpe.com>
2022-08-12 18:46:30 +0530

    Added Strong Attack Functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK = 11;
 const PLAYER_VAUE = 10;
 const chosenHealth = 100;
 let playerHealth = chosenHealth;
@@ -18,4 +19,18 @@ function attackHandler() {
     alert("Draw");
   }
 }
+function strongAttack() {
+  const damageMonster = dealMonsterDamage(STRONG_ATTACK);
+  monsterHealth -= damageMonster;
+  const damagePlayer = dealPlayerDamage(PLAYER_VAUE);
+  playerHealth -= damagePlayer;
+  if (monsterHealth <= 0 && playerHealth > 0) {
+    alert("You Won");
+  } else if (monsterHealth > 0 && playerHealth <= 0) {
+    alert("You Lost");
+  } else if (monsterHealth <= 0 && playerHealth <= 0) {
+    alert("Draw");
+  }
+}
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttack);
